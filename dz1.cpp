b675a320855ced3ee957e3154709c976d6b90da1
Unknown <ponomarchuk1313@mail.ru>
2020-10-03 11:35:57 +0300

    dz1

---
+++ dz1.cpp
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#include <conio.h>
+#include <math.h>
+#pragma hdrstop
+
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int main(int argc, char* argv[])
+{
+    double x,y,z,a,b,c,rez;
+    puts("\n\tx,y,z=");
+    scanf("%lf%lf%lf", &x,&y,&z);
+    a=pow(log10(y),sqrt(|x|));
+    b=(x-y\2);
+    c=(pow(sin))*atan(z);
+    rez=a*b-c;
+    printf("\n x=lf\n y=lf\n z=lf\n Rezult=%lf\n",x,y,z,rez);
+    puts("Press any key...");
+    getch();
+    return 0;
+}
+//---------------------------------------------------------------------------
+ 
